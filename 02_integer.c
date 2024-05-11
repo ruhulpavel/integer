b4c65a5ac2fe6ed1b883778eb9446d7cf1b5c66c
Ruhul Amin <82967538+ruhulpavel@users.noreply.github.com>
2024-05-11 15:22:02 +0600

    Add files via upload
    C Program

---
+++ 02_integer.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main ()
+{
+	int firstNum, secondNum;
+	printf ("Enter first number :");
+	scanf ("%d", &firstNum);
+	printf("Enter second number :");
+	scanf ("%d", &secondNum);
+	printf ("Answer : %d", firstNum + secondNum);
+	
+	return 0;
+}
